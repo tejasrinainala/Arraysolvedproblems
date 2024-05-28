0f8b02610878f710c525edab6edc9e455b26d649
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-05-28 19:38:24 +0530

    Create p2.js

---
+++ p2.js
@@ -0,0 +1,14 @@
+var runningSum = function(nums) {
+    for(let i=0;i<nums.length;i++)
+    {
+        if(i==0)
+        {
+            nums[i]=nums[i]
+        }
+        else
+        {
+            nums[i]=nums[i-1]+nums[i]
+        }
+    }
+    return nums;
+};
