bf545e0a064872f38aab7f1d355ff45be1967097
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-04 20:05:34 +0530

    Update final_prices_with_special_discount.js

--- final_prices_with_special_discount.js
+++ final_prices_with_special_discount.js
@@ -1,9 +1,11 @@
 var finalPrices = function(prices) {
+    //creating a new array for the final price.
     let fprice=prices.slice();
     for(let i=0;i<prices.length;i++)
     {
         for(let j=i+1;j<prices.length;j++)
         {
+            //verifying the condition and adding to the final resultant array.
             if(prices[j]<=prices[i])
             {
                 fprice[i] = Math.abs(prices[i]-prices[j])
@@ -16,5 +18,6 @@ var finalPrices = function(prices) {
             
         }
     }
+    //printing the array of final prices.
     return fprice;
 };
